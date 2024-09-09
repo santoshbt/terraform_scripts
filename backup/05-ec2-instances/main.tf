ee4f7bf291b0cb99ce05886ecd141596862b75a8
Santosh Turamari <bt.santosh01@gmail.com>
2024-09-09 18:04:09 +0100

    create ec2 instances

---
+++ backup/05-ec2-instances/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.46"
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+// HTTP Server -> SG
+// SG -> 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  # vpc_id = "vpc-09cea7f59d940486a"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  # ami                  = "ami-0a5c3558529277641"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  # subnet_id              = "subnet-072806716fffc51f4"
+  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[1]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",                                                                                                // install httpd
+      "sudo service httpd start",                                                                                                 // start
+      "echo 'Welcome to Santosh terraform lessons - Virtual server is at ${self.public_dns}' | sudo tee /var/www/html/index.html" // copy a file
+    ]
+  }
+}
+
+
